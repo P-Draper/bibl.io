b65b529743b17a2260dfd03edb9c4f42992e4abf
P-Draper <draperpa@gmail.com>
2023-09-17 21:01:25 -0400

    more pipeline work

--- flask/app.py
+++ flask/app.py
@@ -2,9 +2,10 @@ from flask_migrate import Migrate
 from flask import Flask, request, make_response, jsonify
 import os
 from pymongo import MongoClient
-from bson import ObjectId  # Import ObjectId from bson
+from bson import ObjectId
 from dotenv import load_dotenv
 import logging
+import subprocess
 
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
@@ -18,39 +19,34 @@ app.json.compact = False
 
 MONGO_URI = os.environ.get("MONGO", '')
 client = MongoClient(MONGO_URI)
-db = client.Test  # Use the "test" database
-
-# Global variable to store the current URL
+db = client.Test 
 current_url = None
 
 @app.route('/getMostRecentUrl', methods=['GET'])
 def get_most_recent_url():
-    global current_url  # Access the global variable
+    global current_url 
 
     try:
         collection = db.urls
         most_recent_url = collection.find_one({}, sort=[('createdAt', -1)])
-        
+
         if most_recent_url:
-            # Convert ObjectId to string
             most_recent_url['_id'] = str(most_recent_url['_id'])
-            
-            # Update the current_url variable
             current_url = most_recent_url['Url']
-            
             logging.debug("Most Recent URL: %s", most_recent_url)
+            subprocess.run(["python", "converter.py", current_url])
+
             return jsonify(most_recent_url), 200
         else:
             logging.debug("No data found")
             return jsonify({"error": "No data found"}), 404
     except Exception as e:
-        logging.error("Error: %s", str(e))  # Use logging.error instead of logging.exception
+        logging.error("Error: %s", str(e))
         return jsonify({"error": str(e)}), 500
 
-# Route to get the current URL value
 @app.route('/getCurrentUrl', methods=['GET'])
 def get_current_url():
-    global current_url  # Access the global variable
+    global current_url
     if current_url:
         return jsonify({"current_url": current_url}), 200
     else:
--- flask/converter.py
+++ flask/converter.py
@@ -1,16 +1,18 @@
 from pytube import YouTube
 import os
+import sys
+
+# Get the URL from command-line arguments
+url = sys.argv[1]
 
 # url input from user
-yt = YouTube(
-    str(input("Enter the URL of the video you want to download: \n>> ")))
+yt = YouTube(url)
 
 # extract only audio
 video = yt.streams.filter(only_audio=True).first()
 
 # check for destination to save file
-print("Enter the destination (leave blank for current directory)")
-destination = str(input(">> ")) or '.'
+destination = str()
 
 # download the file
 out_file = video.download(output_path=destination)
@@ -19,6 +21,3 @@ out_file = video.download(output_path=destination)
 base, ext = os.path.splitext(out_file)
 new_file = base + '.mp3'
 os.rename(out_file, new_file)
-
-# result of success
-print(yt.title + " has been successfully downloaded.")
